a29fb4f65587771ea87e34a591dcf766bf44d69d
leehjhjhj <tsukiakarii@naver.com>
2023-07-25 12:29:48 +0900

    build: RDS 연동 및 pymysql 의존성 추가

--- hackathon/settings/base.py
+++ hackathon/settings/base.py
@@ -68,12 +68,6 @@ WSGI_APPLICATION = 'hackathon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
--- hackathon/settings/prod.py
+++ hackathon/settings/prod.py
@@ -1,4 +1,19 @@
 from .base import *
+from decouple import config
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
