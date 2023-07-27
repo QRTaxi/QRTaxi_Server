986a80974e249bb658f30d725641477c26cea577
leehjhjhj <tsukiakarii@naver.com>
2023-07-27 14:54:48 +0900

    chore: deploy.sh 삭제, main.yml 롤백

--- .github/deploy.sh
+++
@@ -1,5 +0,0 @@
-docker stop $(docker ps -a -q) 
-docker rm $(docker ps -a -q)
-cd /home/ec2-user/
-docker-compose pull  # Pull the latest images on EC2
-docker-compose up -d  # Run the containers on EC2
