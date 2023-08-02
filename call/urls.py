bdc8a1f4551d3aa5ea01ec38504ff21c19d4e528
leehjhjhj <tsukiakarii@naver.com>
2023-08-02 21:35:45 +0900

    HOTFIX: call url import 추가

--- call/urls.py
+++ call/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from call.views import CallMainView
+from test.views import liveblog_index
 app_name = 'call'
 
 urlpatterns = [
-    path("wstest/<str:post_pk>/", views.liveblog_index),
+    path("wstest/<str:post_pk>/", liveblog_index),
     path('main/<str:hashed_qr_id>', CallMainView.as_view()),
 ]
