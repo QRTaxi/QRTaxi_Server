a1194d9256c7f37480ff0bc3a04ff8d9417b30cc
yoojung.choi <green980611@naver.com>
2023-08-04 20:34:47 +0900

    refactor: dataerror잡기, id를 driver_id로 수정

--- driver/services/driver_location_service.py
+++ driver/services/driver_location_service.py
@@ -4,13 +4,16 @@ def get_driver_location(data):
     """
     택시 기사님 위치 저장하는 Service
     """
-    driver_id = data.get('id')
+    driver_id = data.get('drive_id')
     latitude = data.get('latitude')
     longitude = data.get('longitude')
     redis_conn = get_redis_connection()
 
+    if not all([driver_id, latitude, longitude]):
+        return {'status': 'ERROR', 'message': '잘못된 입력값이 있습니다. 모든 필드를 제대로 채웠는지 확인해주세요.'}
+
     if redis_conn is None:
-        return {'status': 'ERROR'}
+        return {'status': 'ERROR', 'message': '레디스 연결에 실패하셨습니다.'}
 
     locations = (latitude, longitude, driver_id)
     redis_conn.geoadd("driver_location", locations)
