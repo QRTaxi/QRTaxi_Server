536e4afa5fed391338fa890ed6b382a67e806296
yoojung.choi <green980611@naver.com>
2023-08-06 21:53:40 +0900

    refactor: docker 수정

--- hackathon/celery.py
+++ hackathon/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackathon.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackathon.settings.prod')
 
 app = Celery('hackathon')
 app.config_from_object('django.conf:settings', namespace='CELERY')
