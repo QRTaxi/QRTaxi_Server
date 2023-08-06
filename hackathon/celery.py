c10fc565eb9e42fd61306db0a47187e893da5f84
yoojung.choi <green980611@naver.com>
2023-08-06 14:24:40 +0900

    refactor: 주석처리

--- hackathon/celery.py
+++ hackathon/celery.py
@@ -9,6 +9,6 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 #테스트할 때만 주석해제하고 실제로 배포할 때는 주석처리하기
-@app.task(bind=True, ignore_result=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+# @app.task(bind=True, ignore_result=True)
+# def debug_task(self):
+#     print(f'Request: {self.request!r}')
