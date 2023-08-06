a3b4512a758267594399dc464032704275aa9591
yoojung.choi <green980611@naver.com>
2023-08-06 16:39:58 +0900

    refacotr: permission_classes 추가

--- call/services/receive_status_service.py
+++ call/services/receive_status_service.py
@@ -1,19 +1,18 @@
 from call.models import Assign
 from driver.models import CustomDriver
 
-def get_accept_status(data):
+def get_accept_status(data, driver):
     """
     수락 요청 시 해당 assign의 status를 success로 바꾸고 driver_id도 업데이트하기
     """
-    driver_id = data.get('driver_id')
     assign_id = data.get('assign_id')
     accepted = data.get('accepted')
     get_assign_info = Assign.objects.get(id=assign_id)
-    get_driver_info = CustomDriver.objects.get(id=driver_id)
+    get_driver_info = CustomDriver.objects.get(id=driver.id)
 
     if accepted:
         get_assign_info.status = 'success'
-        get_assign_info.driver_id = get_driver_info
+        get_assign_info.driver_id = driver
         get_driver_info.is_able = False
         get_assign_info.save(update_fields=['status', 'driver_id'])
         get_driver_info.save(update_fields=['is_able'])
--- call/views/receive_status_view.py
+++ call/views/receive_status_view.py
@@ -1,15 +1,18 @@
-from rest_framework.response import Response
-from rest_framework import status, exceptions
-from rest_framework.views import APIView
 from call.services import *
+from rest_framework import exceptions, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 class ReceiveAcceptView(APIView):
+    permission_classes = [IsAuthenticated]
     """
     기사님이 수락 요청하는 view
     """
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         try:
-            response = get_accept_status(request.data)
+            response = get_accept_status(request.data, request.user)
             return Response(response, status=status.HTTP_200_OK)
         except exceptions.ValidationError:
             return Response({"statusCode": 400, "detail": "잘못된 요청입니다."}, status=status.HTTP_400_BAD_REQUEST)
