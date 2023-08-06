4d631d69001d5ee569ccdf9c9bf3fb980f1a3b4f
yoojung.choi <green980611@naver.com>
2023-08-06 23:37:17 +0900

    refactor: start.sh 수정

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 python manage.py collectstatic --no-input
-daphne -b 0.0.0.0 -p 8000 hackathon.asgi:application
+daphne -b 0.0.0.0 -p 8000 hackathon.asgi:application &
 celery -A hackathon worker --loglevel=info
