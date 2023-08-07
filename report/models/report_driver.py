1478ed3c726bdfdda98d6dee6c94e7330e883667
yoojung.choi <green980611@naver.com>
2023-08-07 23:48:15 +0900

    refactor: report 모델에 contents 추가

--- report/models/report_driver.py
+++ report/models/report_driver.py
@@ -7,3 +7,4 @@ class ReportDriver(models.Model):
     assign_id = models.ForeignKey(Assign, on_delete=models.CASCADE, verbose_name="신고한 유저")
     driver_id = models.ForeignKey(CustomDriver, on_delete=models.CASCADE, verbose_name="신고당한 기사님")
     report_time = models.DateTimeField(auto_now_add=True)
+    contents = models.TextField(help_text="신고 내용을 작성해주세요.", blank=True)
