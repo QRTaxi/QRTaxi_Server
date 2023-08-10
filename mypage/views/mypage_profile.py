a49396741a24cf1981fb8dd5b591d9e8659cc3d9
dh65a02 <dh65a02@likelion.org>
2023-08-10 16:32:49 +0900

    feat: views 수정

--- mypage/views/mypage_profile.py
+++ mypage/views/mypage_profile.py
@@ -35,5 +35,5 @@ class MypageDetailView(APIView):
         serializer = MypageSerializer(driver, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response({'statusCode': status.HTTP_200_OK, 'data':serializer.data})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- report/views/report_user_views.py
+++ report/views/report_user_views.py
@@ -17,5 +17,5 @@ class ReportUserView(APIView):
         serializer = ReportUserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({'statusCode': status.HTTP_201_CREATED, 'data':serializer.data})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
