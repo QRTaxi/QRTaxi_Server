0880ccd42bee4e3baa4f9a5b28f2b4821ae82a42
leehjhjhj <tsukiakarii@naver.com>
2023-08-11 13:13:57 +0900

    feat: 상태 조회를 위한 api 작성

---
+++ call/views/call_request_status_view.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from rest_framework import status, exceptions
+from rest_framework.views import APIView
+from call.services import response_status
+
+
+class CallRequestStatusView(APIView):
+    """
+    소켓이 끊겼을 때, 해당 assign의 status를 조회하여 response 보내주는 view
+    """
+    def post(self, request):
+        try:
+            response = response_status(request.data)
+            return Response(response, status=status.HTTP_201_CREATED)
+        except exceptions.ValidationError as e:
+             return Response({"detail": "잘못된 요청입니다.", "error": e.detail}, status=status.HTTP_400_BAD_REQUEST)
