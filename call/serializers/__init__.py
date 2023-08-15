8aa4cb23cba8452db2d80e8637cc23ef14a59e06
leehjhjhj <tsukiakarii@naver.com>
2023-08-16 00:15:58 +0900

    feat: 푸시 알람 추가

--- call/serializers/__init__.py
+++ call/serializers/__init__.py
@@ -1,3 +1,4 @@
 from .call_serializer import *
 from .call_success_serializer import *
 from .receive_serializer import *
+from .call_push_token_serializer import *
---
+++ call/serializers/call_push_token_serializer.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class CallPushTokenSerializer(serializers.Serializer):
+    assign_id = serializers.CharField(max_length=10)
+    push_token = serializers.CharField(max_length=256) 
