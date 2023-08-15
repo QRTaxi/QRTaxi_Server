52962cfe0e6d6b8ac0a2a621b05cd2c45afa9534
leehjhjhj <tsukiakarii@naver.com>
2023-08-15 17:19:23 +0900

    feat: 성공페이지 기사 폰번호 추가

--- call/serializers/call_success_serializer.py
+++ call/serializers/call_success_serializer.py
@@ -3,10 +3,10 @@ from call.models import Assign
 from driver.models import CustomDriver
 
 class CallSuccessDriverInfoSerializer(serializers.ModelSerializer):
-    
+    driver_phone = serializers.CharField(source='phone_num', read_only=True)
     class Meta:
         model = CustomDriver
-        fields = ("id" ,"name", "taxi_num", "car_type",)
+        fields = ("id" ,"name", "taxi_num", "car_type", "driver_phone")
 
 class CallSuccessGetSerializer(serializers.ModelSerializer):
     driver_id = CallSuccessDriverInfoSerializer()
